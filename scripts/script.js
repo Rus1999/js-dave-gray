71b9372923223d95e469fb3d0ea9a8ac079d315c
Punyarit Klaphachon <punyarit07@gmail.com>
2024-03-03 21:10:52 +0700

    simple function

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,8 @@
-let name = "Punyarit";
-let counter = 0;
-let myLetter;
-while (counter <= name.length - 1)
+// function declaration syntax;
+
+function add2Num()
 {
-  myLetter = name[counter];
-  console.log(myLetter);
-  if (counter === 1)
-  {
-    counter += 2;
-    continue;
-  }
-  if (myLetter === "t") break;
-  counter++;
+  return 2 + 2;
 }
 
-console.log(counter);
+console.log(add2Num());
