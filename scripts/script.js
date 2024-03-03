bfc3fa41cc995915ce4d83df13dbda1ac87e7465
Punyarit Klaphachon <punyarit07@gmail.com>
2024-03-03 21:09:06 +0700

    finish loop chapter

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
+let name = "Punyarit";
 let counter = 0;
 let myLetter;
-while (true)
+while (counter <= name.length - 1)
 {
   myLetter = name[counter];
   console.log(myLetter);
-  if (myLetter === "v") break;
+  if (counter === 1)
+  {
+    counter += 2;
+    continue;
+  }
+  if (myLetter === "t") break;
   counter++;
 }
+
+console.log(counter);
