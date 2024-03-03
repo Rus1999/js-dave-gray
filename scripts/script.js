b9c771bdec249ed5e329bf0178353651b160f781
Punyarit Klaphachon <punyarit07@gmail.com>
2024-03-03 21:13:25 +0700

    test log augrument that have been provid on a function call

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@
 
 function add2Num(num1, num2)
 {
+  console.log(`Numeric 1: {num1}`);
+  console.log(`Numeric 2: {num2}`);
   return num1 + num2;
 }
 
-console.log(add2Num(3, 4));
+console.log(add2Num(3, 7));
