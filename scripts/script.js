e1b54d9813bcf2ed6e5110a9f98ebbfac6f25c9f
Punyarit Klaphachon <punyarit07@gmail.com>
2024-03-03 21:14:00 +0700

    fix string interpolation template

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@
 
 function add2Num(num1, num2)
 {
-  console.log(`Numeric 1: {num1}`);
-  console.log(`Numeric 2: {num2}`);
+  console.log(`Numeric 1: ${num1}`);
+  console.log(`Numeric 2: ${num2}`);
   return num1 + num2;
 }
 
