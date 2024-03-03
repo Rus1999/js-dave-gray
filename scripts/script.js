6bba5a20d2f16dbb28a712c308927a3657486f8f
Punyarit Klaphachon <punyarit07@gmail.com>
2024-03-03 21:22:37 +0700

    arrow function declaration

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
-const getUserNameFromEmail = function (email)
+const getUserNameFromEmail = (email) =>
 {
   return email.slice(0, email.indexOf("@"));
 }
 
 console.log("Input email: Punyarit07@gmail.com");
 console.log(getUserNameFromEmail("Punyarit07@gmail.com"));
+
+console.log("Input email: Punyarit_rus@hotmail.com");
+console.log(getUserNameFromEmail("Punyarit_rus@hotmail.com"));
+
+console.log("Input email: PunyaritKlaphachon@github.com");
+console.log(getUserNameFromEmail("PunyaritKlaphachon@github.com"));
