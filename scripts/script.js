973ef2f6a5f70e233bac2c8d21d676839dd29ca7
Rus@home <punyarit07@gmail.com>
2024-03-05 21:45:03 +0700

    parent-child class

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
 class Pizza {
-  constructor (pizzaType, pizzaSize){
-    this.type = pizzaType;
+  constructor (pizzaSize){
     this.size = pizzaSize;
     this.crust = "original";
-    this.toppings = [];
-  };
+  }
 
   getCrust() {
     return this.crust;
@@ -13,28 +11,19 @@ class Pizza {
   setCrust(pizzaCrust) {
     this.crust = pizzaCrust;
   }
-
-  getToppings() {
-    return this.toppings;
-  }
-
-  setToppings(topping) {
-    this.toppings.push(topping);
-  }
-
-  bake() {
-    console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`);
-  };
 }
 
-const myPizza = new Pizza("havaian", "small");
-myPizza.setCrust("thin");
-myPizza.setToppings("sausage");
-myPizza.setToppings("olives");
-myPizza.bake();
-console.log(myPizza.getToppings());
+class SpecialtyPizza extends Pizza {
+  constructor (pizzaSize){
+    super(pizzaSize);
+    this.type = "The Works";
+  }
 
-const currentTimeMs = new Date().getTime();
-const currentTimeDate = new Date(currentTimeMs);
+  slice() {
+    console.log(`Our ${this.type} ${this.size} pizza`);
+  }
+}
 
-console.log(currentTimeDate);
+const mySpecialtyPizza = new SpecialtyPizza("Large");
+mySpecialtyPizza.slice();
+console.log(mySpecialtyPizza.getCrust());
