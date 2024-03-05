33199e9fbd934b4159f6f76b7f389becac7cba9d
Rus@home <punyarit07@gmail.com>
2024-03-05 21:10:10 +0700

    object with function and variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,31 @@
-const vehicle = {
-  wheels : 4,
-  engine : () => {
-    return "Vrroooom!";
-  }
+const band = {
+  vocals: "Robert Plant",
+  guitar: "Jimmy Page",
+  bass: "John Paul Jones",
+  drums: "John Bonham"
+};
+
+delete band.drums;
+console.log(band.hasOwnProperty("drums"));
+band.drums = "John Bonham";
+
+console.log(Object.keys(band));
+console.log(Object.values(band));
+
+for (let job in band) {
+  console.log(`On ${job}, it's ${band[job]}`);
 }
 
-const car = Object.create(vehicle);
-car.doors = 7;
-car.engine = () => { return "Whoooosh! " }
-console.log(car.doors);
-console.log(car.engine());
+// const { guitar: myVariable, bass: myBass } = band; // get value from key
+// console.log(myVariable);
+// console.log(myBass);
 
-const testla = Object.create(car);
-console.log(testla.wheels);
-console.log(testla.doors);
-console.log(testla.engine());
-testla.engine = () => { return "Shhhh....." }
-console.log(testla.engine());
+// {
+  const { bass } = band; // get value from key
+  console.log(bass);
+// }
+
+function sings({ vocals, guitar, bass, drums }) {
+  return `${vocals} sings! \n${guitar} lead! \n${bass} pick!\n${drums} fucking hit!`;
+};
+console.log(sings(band)); // pass the whole object then use what to use inside the object
