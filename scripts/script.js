c3c65d6d75e90731ed21d50c2c44acd6f6a5caa2
Rus@home <punyarit07@gmail.com>
2024-03-10 22:53:00 +0700

    try catch

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,54 @@
-const myObj = {
-  name: "Rus",
-  hobbies: ["all five sin", "bad habit", "coding"],
-  hello: () => {
-    console.log("Hello!");
+"use strict";
+
+const variable = "Rus";
+console.log(variable);
+
+const name = "Punyarit";
+
+const makeError = () => {
+  let i = 0;
+
+  while (i <= 5) {
+    try {
+      // const name2 = "Rus";
+      // name2 = "punyarit";
+
+      // throw new customError("This is a custom error!");
+
+      // throw new Error("This is a genetic eror");
+
+      if (i % 2 !== 0) {
+        throw new Error("Odd number!"); // throw an error then skip to the catch block
+      }
+
+      console.log("Even number");
+    } catch (err) { // only execute when error occurs
+      // console.log(err);
+      // console.error(err);
+      // console.warn(err);
+      // console.table(err);
+      // console.error(err.name);
+      // console.error(err.message);
+      // console.error(err.stack);
+      // logTheError(err.stack); // record the log 
+
+      // console.error(err.name);
+      // console.error(err.message);
+      console.error(err.stack);
+    } finally {
+      // execute no matter what
+      // console.log("WOWZAA!");
+      console.log("FINALLY");
+      i++;
+    }
   }
 };
 
-console.log(myObj);
-myObj.hello();
+makeError();
 
-console.log(typeof(myObj));
 
-const sendJSON = JSON.stringify(myObj); 
-console.log(sendJSON);
-console.log(typeof sendJSON); // string
-console.log(sendJSON.name); // return undefined
-
-const receiveJSON = JSON.parse(sendJSON);
-console.log(receiveJSON.name);
-console.log(typeof receiveJSON); // object
+const customError = (message) => {
+  this.message = message;
+  this.name = "customError";
+  this.stack = `${this.name}: ${this.message}`;
+};
