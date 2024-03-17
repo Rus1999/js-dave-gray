d62e1e7652186066a02ce537d74fa9766ad5d3f3
Rus@home <punyarit07@gmail.com>
2024-03-17 21:11:08 +0700

    preventDafult behaviors

--- scripts/script.js
+++ scripts/script.js
@@ -22,49 +22,10 @@ document.addEventListener("readystatechange", (e) => {
 }, false);
 
 const initApp = () => {
-  const view = document.querySelector("#view2");
-  const div = view.querySelector("div");
-  const h2 = div.querySelector("h2");
-
-  // if useCapture is false then it will propagate from inside out
-  // if it's true then it will outside in
-  view.addEventListener("click", (e) => {
-    view.classList.toggle("purple");
-    view.classList.toggle("darkblue");
-  }, false);
-
-  div.addEventListener("click", (e) => {
-    div.classList.toggle("blue");
-    div.classList.toggle("black");
-  }, false);
-
-  h2.addEventListener("click", (e) => {
-    const myText = e.target.textContent;
-    myText === "My 2nd View"
-      ? e.target.textContent = "Clicked"
-      : e.target.textContent = "My 2nd View";
-  }, false);
-
-
-  // the event will propaget back up to the parent
-  // e.target is the element that we click
-  // view.addEventListener("click", (e) => {
-  //   e.target.style.backgroundColor = "purple";
-  // }, false);
-
-  // div.addEventListener("click", (e) => {
-  //   e.target.style.backgroundColor = "blue";
-  // }, false);
-
-  // h2.addEventListener("click", (e) => {
-  //   e.target.textContent = "clicked";
-  // }, false);
-
-  const nav = document.querySelector("nav");
-  nav.addEventListener("mouseover", (e) => {
-    e.target.classList.add("height100");
-  }, false);
-  nav.addEventListener("mouseout", (e) => {
-    e.target.classList.remove("height100");
-  }, false);
+  const view3 = document.querySelector("#view3");
+  const myForm = view3.querySelector("#myForm");
+  myForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // prevent form to reload by defaul
+    console.log("Submit Event.");
+  })
 };
