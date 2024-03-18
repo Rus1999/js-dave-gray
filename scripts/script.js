921c99f97d39636b6d7679d1266fa12174c08ef5
Rus@home <punyarit07@gmail.com>
2024-03-18 20:13:33 +0700

    callback (legacy)

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,6 @@
-import { posts } from "./posts.js";
+// calbacks
 
-posts.forEach(post => {
-  console.log(post);
-});
-console.clear();
-
-const filteredPosts = posts.filter(post => {
-  return post.userId === 7;
-});
-console.log(filteredPosts);
-console.clear();
-
-const mappedPosts = filteredPosts.map(post => {
-  return post.id * 10;
-});
-console.log(mappedPosts);
-
-const reducedPostsValue = mappedPosts.reduce((sum, post) => {
-  return sum + post;
-}) ;
-console.log(reducedPostsValue)
+function firstFunction(parameters, callback){
+  // do stuff
+  callback();
+}
