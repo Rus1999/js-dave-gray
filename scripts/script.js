3aca02a0ceadbfcf00136881de877f1f05fca235
Rus@home <punyarit07@gmail.com>
2024-03-19 18:44:57 +0700

    promise example

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,52 @@
-// calbacks
+// promises (do multiple tasak at once)
+// 3 state : pending, fufilled, rejected
 
-function firstFunction(parameters, callback){
-  // do stuff
-  callback();
-}
+const myPromise = new Promise((resolve, reject) => {
+  const error = false;
+  if (!error) { // error == false
+    resolve("Yes! resolved the promise!");
+  } else { // error != false
+    reject("No! reject the promise!");
+  }
+});
+
+// console.log(myPromise); // reture state of the promise
+
+// myPromise.then(value => { // value = resolve || reject
+//   console.log(`1st`); 
+//   return `2nd`;
+// })
+// .then(newValue => {
+//   console.log(newValue);
+//   return '3rd';
+// })
+// .then(newValue => {
+//   throw new Error('WOWZAA');
+//   console.log(newValue);
+// })
+// .catch(err => {
+//   console.error(err);
+// });
+
+
+const myNextPromise = new Promise((resolve, reject) => {
+  setTimeout(function() {
+    resolve("myNextPromise resolved!");
+  }, 3000);
+});
+
+myNextPromise.then((value) => {
+  console.log(value);
+})
+.catch((err) => {
+  console.stack(err);
+});
+
+
+myPromise.then((value) => {
+  console.log(`This promise source code is below the myNextPromise line`);
+  console.log(value);
+})
+.catch((error) => {
+  console.stack(err);
+});
