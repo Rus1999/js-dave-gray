d05a563dc2f2734320d5163ded2d691e8720c8fe
Rus@home <punyarit07@gmail.com>
2024-03-19 20:47:56 +0700

    async / await example5 with abstract into function

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,38 @@
 // Async / Await
 // 2nd parameter of Fetch is a object
 
-const requestJoke = async (lastName) => {
-  const response = await fetch(`https://api.nationalize.io/?name=${lastName}`);
-  const jsonResponse = await response.json();
+// abstract into functions
 
-  console.log(jsonResponse);
+// maybe from a form
+const getDataFromForm = () => {
+  const requestObj = {
+    lastName: "nathaniel"
+  };
+
+  return requestObj;
+}
+
+const buildRequestUrl = (requestData) => {
+  return `https://api.nationalize.io/?name=${requestData.lastName}`;
+}
+
+const requestJoke = async (url) => {
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+  const joke = jsonResponse;
+  postJokeToPage(joke);
 };
 
-requestJoke('nathaniel');
+const postJokeToPage = (joke) => {
+  console.log(joke);
+}
+
+// Procedural "workflow" function
+const processJokeRequest = async () => {
+  const requestData = getDataFromForm();
+  const requestUrl = buildRequestUrl(requestData);
+  await requestJoke(requestUrl);
+  console.log("finished!");
+}
+
+processJokeRequest();
