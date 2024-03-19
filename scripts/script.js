8fa79a9ac9fbeb87e7a857dcf814a1780125d94d
Rus@home <punyarit07@gmail.com>
2024-03-19 19:19:41 +0700

    promise example2

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,21 @@
-// promises (do multiple tasak at once)
-// 3 state : pending, fufilled, rejected
+// fetch
 
-const myPromise = new Promise((resolve, reject) => {
-  const error = false;
-  if (!error) { // error == false
-    resolve("Yes! resolved the promise!");
-  } else { // error != false
-    reject("No! reject the promise!");
-  }
-});
+// const users = fetch("https://jsonplaceholder.typicode.com/users"); // promise by default
 
-// console.log(myPromise); // reture state of the promise
+// pending
+// console.log(users);
 
-// myPromise.then(value => { // value = resolve || reject
-//   console.log(`1st`); 
-//   return `2nd`;
-// })
-// .then(newValue => {
-//   console.log(newValue);
-//   return '3rd';
-// })
-// .then(newValue => {
-//   throw new Error('WOWZAA');
-//   console.log(newValue);
-// })
-// .catch(err => {
-//   console.error(err);
-// });
+const users = fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    json.forEach(user => {
+      console.log(user);
+    })
+  })
+  .catch(err => {
+    console.stack(err);
+  });
 
-
-const myNextPromise = new Promise((resolve, reject) => {
-  setTimeout(function() {
-    resolve("myNextPromise resolved!");
-  }, 3000);
-});
-
-myNextPromise.then((value) => {
-  console.log(value);
-})
-.catch((err) => {
-  console.stack(err);
-});
-
-
-myPromise.then((value) => {
-  console.log(`This promise source code is below the myNextPromise line`);
-  console.log(value);
-})
-.catch((error) => {
-  console.stack(err);
-});
+console.log(users);
