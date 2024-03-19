5617db9856194f2917e7fd7ea809e27ed22f50ff
Rus@home <punyarit07@gmail.com>
2024-03-19 20:27:24 +0700

    async / awiat example4 with get with specify parameter on url

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,11 @@
 // Async / Await
 // 2nd parameter of Fetch is a object
 
-const jokeObj = {
-  id: "3EYLeVS7Me", 
-  joke: "I once lost a banana at court but then I appealed."
-}
-
-const postData = async (jokeObj) => {
-  const response = await fetch("https://httpbin.org/post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(jokeObj)
-    });
-
+const requestJoke = async (lastName) => {
+  const response = await fetch(`https://api.nationalize.io/?name=${lastName}`);
   const jsonResponse = await response.json();
 
   console.log(jsonResponse);
 };
 
-postData(jokeObj);
+requestJoke('nathaniel');
